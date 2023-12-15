4e2c8b2ede33288a6ebef54a730f80ec1df845b5
Poparded <71463217+Poparded@users.noreply.github.com>
2023-12-15 15:16:52 +0100

    e

--- Task 3.py
+++ Task 3.py
@@ -1,21 +1,28 @@
 import numpy as np
+import time
+
+start_time = time.time()
+
 number = 0
 # Generating random 2D matrices A1, A2, B1, B2 each with dimensions 250x250
 # and elements being positive integers less than 100
 while number <= 1:
     A1 = np.random.randint(1, 100, (250, 250))
-   # print(A1)
+    # print(A1)
     A2 = np.random.randint(1, 100, (250, 250))
-    #print(A2)
+    # print(A2)
 
     B1 = np.random.randint(1, 100, (250, 250))
-    #print(B1)
+    # print(B1)
 
     B2 = np.random.randint(1, 100, (250, 250))
-    #print(B1)
+    # print(B1)
 
     Z1 = np.multiply(A1, B1)
     Z2 = np.multiply(A2, B2)
     Z = Z1 + Z2
     print(Z)
     number += 1
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f" Exection time: {execution_time} Seconds")
